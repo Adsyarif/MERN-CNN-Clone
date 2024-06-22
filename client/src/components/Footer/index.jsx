c203a8c056d97e33982fd09fc7d18a6cca0c4104
Adsyarif <adrisansyarif@gmail.com>
2024-06-22 17:48:59 +0700

    BREAKING CHANGE: category list and search input in footer

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,7 +1,138 @@
+import { NavLink } from "react-router-dom";
+
 const Footer = ({ serachText }) => {
   const handleInputChange = (event) => {
     console.log(event.target.value);
   };
+
+  const categoriesRow1 = [
+    {
+      title: "World",
+      items: [
+        "Africa",
+        "Americas",
+        "Asia",
+        "Australia",
+        "China",
+        "Europe",
+        "India",
+        "Middle East",
+        "United Kingdom",
+      ],
+    },
+    {
+      title: "US Politics",
+      items: ["The Biden Presidency", "Facts First", "2024 Elections"],
+    },
+    {
+      title: "Business",
+      items: ["Markets", "Tech", "Media", "Calculators", "Videos"],
+    },
+    {
+      title: "Health",
+      items: [
+        "Life, But Better",
+        "Fitness",
+        "Food",
+        "Sleep",
+        "Mindfulness",
+        "Relationships",
+      ],
+    },
+    {
+      title: "Entertainment",
+      items: ["Movies", "Television", "Celebrity"],
+    },
+    {
+      title: "Tech",
+      items: [
+        "Innovate",
+        "Gadget",
+        "Foreseeable Future",
+        "Mission: Ahead",
+        "Upstarts",
+        "Work Transformed",
+        "Innovative Cities",
+      ],
+    },
+    {
+      title: "Style",
+      items: [
+        "Arts",
+        "Design",
+        "Fashion",
+        "Architecture",
+        "Luxury",
+        "Beauty",
+        "Video",
+      ],
+    },
+
+    // Add other categories similarly
+  ];
+  const categoriesRow2 = [
+    {
+      title: "Travel",
+      items: ["Destinations", "Food & Drink", "Stay", "News", "Videos"],
+    },
+    {
+      title: "Sports",
+      items: [
+        "Football",
+        "Tennis",
+        "Golf",
+        "Motorsport",
+        "US Sports",
+        "Olympics",
+        "Climbing",
+        "Esports",
+        "Hockey",
+      ],
+    },
+    {
+      title: "Watch",
+      items: [
+        "Live TV",
+        "Digital Studios",
+        "CNN Films",
+        "HLN",
+        "TV Schedule",
+        "TV Shows A-Z",
+        "CNNVR",
+      ],
+    },
+    {
+      title: "Features",
+      items: [
+        "As Equals",
+        "Call to Earth",
+        "Freedom Project",
+        "Impact Your World",
+        "Inside Africa",
+        "2 Degrees",
+        "CNN Heroes",
+        "All Features",
+      ],
+    },
+    {
+      title: "Weather",
+      items: ["Climate", "Wildfire Tracker", "Video"],
+    },
+    {
+      title: "More",
+      items: [
+        "Photos",
+        "Longform",
+        "Investigations",
+        "CNN Profiles",
+        "CNN Leadership",
+        "CNN Newsletters",
+        "Work for CNN",
+      ],
+    },
+    // Add other categories similarly
+  ];
+
   return (
     <div className="bg-black p-8">
       <hr className="pb-5" />
@@ -32,7 +163,50 @@ const Footer = ({ serachText }) => {
           </span>
         </button>
       </div>
-      <div></div>
+      <div className="bg-black text-white p-4">
+        <div className="grid grid-cols-7 pb-4">
+          {categoriesRow1.map((category, index) => (
+            <div key={index} className="border-b-3 border-white">
+              <span className="font-bold text-lg">{category.title}</span>
+              <div className="flex flex-col flex-wrap mt-2 ">
+                {category.items.map((item, i) => (
+                  <NavLink
+                    key={i}
+                    to={`/${category.title.toLowerCase()}/${item.toLowerCase()}`}
+                    className={
+                      "py-1 text-sm leading-none hover:underline hover:underline-offset-2"
+                    }
+                    exact
+                  >
+                    {item}
+                  </NavLink>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="grid grid-cols-7 pb-4">
+          {categoriesRow2.map((category, index) => (
+            <div key={index} className="border-b-3 border-white">
+              <span className="font-bold text-lg">{category.title}</span>
+              <div className="flex flex-col flex-wrap mt-2 ">
+                {category.items.map((item, i) => (
+                  <NavLink
+                    key={i}
+                    to={`/${category.title.toLowerCase()}/${item.toLowerCase()}`}
+                    className={
+                      "py-1 text-sm leading-none hover:underline hover:underline-offset-2"
+                    }
+                    exact
+                  >
+                    {item}
+                  </NavLink>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
       <div></div>
     </div>
   );
